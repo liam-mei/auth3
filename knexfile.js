4a382b9bb248de8b49bc7e05af037f43fcda74fe
Derrick Mei <derrick.mei.168@gmail.com>
2020-01-22 18:04:17 -0600

    created knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,60 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./database/auth.db3"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  },
+
+  staging: {
+    client: "postgresql",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./database/auth.db3"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./database/auth.db3"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  }
+};
