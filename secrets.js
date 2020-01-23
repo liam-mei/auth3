3bedf3abdbbfa0d349fcb6adda01bbd13bcd29e4
Derrick Mei <derrick.mei.168@gmail.com>
2020-01-23 01:39:03 -0600

    added secret to env vars

--- secrets.js
+++ secrets.js
@@ -1,4 +1,5 @@
 module.exports = {
-  port : process.env.PORT || 5000,
-  environment: process.env.ENVIRONMENT || 'development'
-}
+  port: process.env.PORT || 5000,
+  environment: process.env.ENVIRONMENT || "development",
+  secret: process.env.SECRET || "I am the default secret"
+};
